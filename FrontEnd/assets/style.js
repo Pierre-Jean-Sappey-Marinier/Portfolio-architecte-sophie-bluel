cfe75fb7b03a60af98e79f128c336c1eadf97034
Pierre-Jean-Sappey-Marinier <pierrejean.sm@gmail.com>
2023-01-24 14:46:25 +0100

    Ajout de photos, avec icones

--- FrontEnd/assets/style.js
+++ FrontEnd/assets/style.js
@@ -156,51 +156,75 @@ if (isDateValid(date) == true) {
 
 // MODALE //
 
-// Get the modal
 var modal = document.getElementById("myModal");
-// Get the button that opens the modal
 var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
 btn.onclick = function () {
   modal.style.display = "block";
 };
 
-// When the user clicks on <span> (x), close the modal
 span.onclick = function () {
   modal.style.display = "none";
 };
 
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
 };
 
-// Ajout du contenu dans la modale !
-
 function afficherImagesDansLaModale(values, categoryId) {
-  console.log("id", categoryId);
+  // console.log("id", categoryId);
 
   for (let i = 0; i < values.length; i++) {
-    // console.log("tableau", values[i].categoryId);
-    // console.log("if", values[i].categoryId == categoryId);
-
     if (values[i].categoryId == categoryId || !categoryId) {
       var figure = document.createElement("figure");
+      figure.classList.add("figure");
+      figure.setAttribute("onmousemove", "apparitionFleche()");
+      figure.setAttribute("onmouseout", "clearCoor()");
       var section_gallery = document.querySelector(".modal-content");
       section_gallery.appendChild(figure);
+
       const imageElement = document.createElement("img");
       imageElement.src = values[i].imageUrl;
+
       imageElement.setAttribute("crossorigin", "anonymous");
       figure.appendChild(imageElement);
+
+      const iconeTrash = document.createElement("div");
+      iconeTrash.classList.add("texte-editer");
+      iconeTrash.innerHTML = '<i class="fa-solid fa-trash-can fa-sm"></i>';
+      const moveIcone = document.createElement("div");
+      moveIcone.classList.add("move_icone");
+
+      moveIcone.innerHTML =
+        '<i class="fa-solid fa-arrows-up-down-left-right "></i>';
+      figure.appendChild(iconeTrash);
+      figure.appendChild(moveIcone);
+
       const auteur = document.createElement("p");
       auteur.innerText = "éditer";
       figure.appendChild(auteur);
     }
   }
+}
+
+// document.getElementsByClassName("moveIcone")[0].style.visibility = "collapse";
+btn.addEventListener("click", function (event) {
+  var hiddenAdmModaleImages = document.getElementsByClassName("move_icone");
+  for (var u = 0; u < hiddenAdmModaleImages.length; u++) {
+    hiddenAdmModaleImages[u].style.visibility = "hidden";
+  }
+});
+
+function apparitionFleche() {
+  var hiddenAdmModaleImages = document.getElementsByClassName("move_icone");
+
+  hiddenAdmModaleImages[0].style.visibility = "visible";
+}
+
+function clearCoor() {
+  var hiddenAdmModaleImages = document.getElementsByClassName("move_icone");
+
+  hiddenAdmModaleImages[0].style.visibility = "hidden";
 }
